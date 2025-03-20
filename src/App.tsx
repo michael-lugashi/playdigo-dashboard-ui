c66fef2d979ef69e3833316a1ee983080bf208c9
michael-lugashi <michael.lugashi@gmail.com>
2025-03-20 15:04:14 +0200

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import './App.css';
+import { Routes, Route } from 'react-router';
+import Login from './components/login/Login';
+import Dashboard from './components/dashboard/Dashboard';
 
 function App() {
-  return <div>hello world</div>;
+  return (
+    <Routes>
+      <Route index element={<Dashboard />} />
+      <Route path="login" element={<Login />} />
+    </Routes>
+  );
 }
 
 export default App;
