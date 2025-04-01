02217c5a99586331fb9f639bc5a57c0adce9dfad
michael-lugashi <michael.lugashi@gmail.com>
2025-04-01 14:55:32 +0300

    moved refresh svg

--- src/components/dashboard/Banner.tsx
+++ src/components/dashboard/Banner.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import playdigoLogo from '../../assets/images/playdigo_logo.jpeg';
+import refreshIcon from '../../assets/icons/refresh.svg';
 
 interface BannerProps {
   lastUpdated: string;
@@ -30,13 +31,7 @@ const Banner: React.FC<BannerProps> = ({ lastUpdated, onRefresh }) => {
           onClick={onRefresh}
           className="px-4 py-2 bg-blue-50 text-blue-600 rounded-full text-sm font-medium hover:bg-blue-100 transition-colors duration-200 flex items-center gap-2"
         >
-          <svg xmlns="http://www.w3.org/2000/svg" className="h-4 w-4" viewBox="0 0 20 20" fill="currentColor">
-            <path
-              fillRule="evenodd"
-              d="M4 2a1 1 0 011 1v2.101a7.002 7.002 0 0111.601 2.566 1 1 0 11-1.885.666A5.002 5.002 0 005.999 7H9a1 1 0 010 2H4a1 1 0 01-1-1V3a1 1 0 011-1zm.008 9.057a1 1 0 011.276.61A5.002 5.002 0 0014.001 13H11a1 1 0 110-2h5a1 1 0 011 1v5a1 1 0 11-2 0v-2.101a7.002 7.002 0 01-11.601-2.566 1 1 0 01.61-1.276z"
-              clipRule="evenodd"
-            />
-          </svg>
+          <img src={refreshIcon} alt="Refresh" className="h-4 w-4" />
           Refresh
         </button>
       </div>
