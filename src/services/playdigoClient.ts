2dc1737c023c658470096aba0ef3bf309306c478
michael-lugashi <michael.lugashi@gmail.com>
2025-04-02 09:08:10 +0300

    changed url to prod

--- src/services/playdigoClient.ts
+++ src/services/playdigoClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const playdigoClient = axios.create({
-  baseURL: 'http://127.0.0.1:3000',
+  baseURL: 'http://16.171.31.249',
   headers: {
     'Content-Type': 'application/json',
   },
