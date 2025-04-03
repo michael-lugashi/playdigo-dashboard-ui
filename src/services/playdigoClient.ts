fed32efe8cd68787c89d2ea538b81125bbfa3607
michael-lugashi <michael.lugashi@gmail.com>
2025-04-03 12:53:59 +0300

    changed routing depending on environment

--- src/services/playdigoClient.ts
+++ src/services/playdigoClient.ts
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
+// Determine if we're running in development mode
+const isDevelopment = import.meta.env.MODE === 'development';
+
 const playdigoClient = axios.create({
-  baseURL: 'http://16.171.31.249',
+  baseURL: isDevelopment ? 'http://localhost:3000' : 'https://api.playdigo-dash.com',
   headers: {
     'Content-Type': 'application/json',
   },
