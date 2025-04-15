9897b615c6cca8f1fcc1cf3ad30e4dfc0bffe27e
michael-lugashi <michael.lugashi@gmail.com>
2025-04-15 15:38:50 +0300

    added admin route

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Login from './components/login/Login';
 import Dashboard from './components/dashboard/Dashboard';
 import AuthProvider from './components/login/AuthProvider';
 import AxiosInterceptor from './components/general/AxiosInterceptor';
+import AdminSettings from './components/adminSettings/AdminSettings';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route index element={<Dashboard />} />
           <Route path="login" element={<Login />} />
+          <Route path="/admin" element={<AdminSettings />} />
         </Routes>
       </AxiosInterceptor>
     </AuthProvider>
